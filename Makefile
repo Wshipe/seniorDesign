fec785399956dd7863b33c815392160c7f3ea51c
Wshipe <wcshipe@gmail.com>
2025-02-16 15:27:21 -0600

    added comments to Makefile and updated project.cpp

--- Makefile
+++ Makefile
@@ -1,11 +1,8 @@
+## Linux Makefile for Freeglut 
 CPPFLAGS=$(shell sdl2-config --cflags) $(EXTRA_CPPFLAGS)
 LDLIBS=$(shell sdl2-config --libs) -lGLEW $(EXTRA_LDLIBS)
-<<<<<<< HEAD
-EXTRA_LDLIBS?=-lGL -lglut
-=======
 EXTRA_LDLIBS?=-lGL -lglut -lGLU
->>>>>>> 5451fea (Menu expample for project.cpp)
-all: project
+all: project ## Main file
 clean:
 	rm -f *.o project
 .PHONY: all clean
--- project.cpp
+++ project.cpp
@@ -1,24 +1,28 @@
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-
-using namespace std;
-
 #include "config.h"
 #include <GL/freeglut_std.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
 
-GLsizei winWidth = 500, winHeight = 500;
+GLfloat xO = 100.0, yO = 50.0, zO = 50.0; // Viewing-coordinate origin
+GLfloat xref = 50.0, yref = 50.0, zref = 0.0; // Look-at point
+GLfloat Vx = 0, Vy = 1.0, Vz = 0.0; // View-up Vector
 
+GLfloat xwMin = -40.0, xwMax = 40.0, ywMin = -60.0, ywMax = 60.0;
+GLfloat dnear = 25.0, dfar = 125.0;
+
+GLsizei winWidth = 600, winHeight = 600;
 GLfloat red = 1.0, green = 1.0, blue = 1.0;
 GLenum renderingMode = GL_SMOOTH;
 
 void init() {
-  glClearColor(.6, .6, .6, 1);
+  glClearColor(red, green, blue, 0);
+
+  glMatrixMode(GL_MODELVIEW);
+  gluLookAt(xO, yO, zO, xref, yref, zref, Vx, Vy, Vz);
 
   glMatrixMode(GL_PROJECTION);
-  gluOrtho2D(0, 300, 0, 300);
+  glFrustum(xwMin, xwMax, ywMin, ywMax, dnear, dfar);
+
 }
 
 void mainMenu (GLint renderingOption) {
@@ -42,27 +46,27 @@ void colorSubMenu (GLint colorOption) {
       red = 0; green = 1; blue = 0;
     break;
     case 3:
-      red = 1; green = 1; blue = 1;
+      red = 1; green = 0; blue = 0;
+    break;
+    case 4:
+      red = 0; green = 0; blue = 0;
   }
   glutPostRedisplay();
 }
 
-/*
-void sphere() {
-  glColor3f(0, 0, 1);
-}
-*/
-
-void displayTriangle () {
+void display() {
   glClear(GL_COLOR_BUFFER_BIT);
 
-  glColor3f(0, green, 0);
-
-  glBegin(GL_TRIANGLES);
-  glVertex2i(280, 20);
-  glVertex2i(160, 280);
-  glColor3f(red, 0, 0);
-  glVertex2i(20, 100);
+  // Set parameters for a square fill area
+  glShadeModel(renderingMode);
+  glColor3f(red, green, blue);
+  glPolygonMode(GL_FRONT, GL_FILL);
+  glPolygonMode(GL_BACK, GL_LINE); // wire-frame back face
+  glBegin(GL_QUADS);
+  glVertex3d(0.0, 0.0, 0.0);
+  glVertex3d(100.0, 0.0, 0.0);
+  glVertex3d(100.0, 100.0, 0.0);
+  glVertex3d(0.0, 100.0, 0.0);
   glEnd();
 
   glFlush();
@@ -85,12 +89,13 @@ int main (int argc, char **argv) {
   glutCreateWindow("Menu Example");
 
   init ();
-  glutDisplayFunc(displayTriangle);
+  glutDisplayFunc(display);
 
   subMenu = glutCreateMenu(colorSubMenu);
     glutAddMenuEntry("Blue", 1);
     glutAddMenuEntry("Green", 2);
-    glutAddMenuEntry("White", 3);
+    glutAddMenuEntry("red", 3);
+    glutAddMenuEntry("black", 4);
 
   glutCreateMenu(mainMenu);
   glutAddMenuEntry("Solid-Color Fill", 1);
@@ -98,8 +103,6 @@ int main (int argc, char **argv) {
   glutAddSubMenu("Color", subMenu);
 
   glutAttachMenu(GLUT_RIGHT_BUTTON);
-
   glutReshapeFunc(reshapeFcn);
-
   glutMainLoop();
 }
